5e77b9bd8061aefd78749acaf17767e94b44f17e
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-04-29 11:26:13 +0200

    Complete task.

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,13 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +18,22 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        String query = "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed ASC";
+        try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            List<String> breeds = new ArrayList<>();
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    breeds.add(rs.getString(1));
+                }
+            }
+            return breeds;
+        } catch (SQLException throwables) {
+            throw new RuntimeException();
+        }
     }
 
 }
